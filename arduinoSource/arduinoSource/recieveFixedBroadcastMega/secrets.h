283d15a66323a168e9668f131fb33c283ed18231
Binnaz DEMİRÇEVİREN <32912389+binnazdemirceviren@users.noreply.github.com>
2021-06-16 17:24:05 +0300

    Delete arduinoSource/arduinoSource directory

--- arduinoSource/arduinoSource/recieveFixedBroadcastMega/secrets.h
+++
@@ -1,4 +0,0 @@
-#define SECRET_MAC {0x90, 0xA2, 0xDA, 0x10, 0x40, 0x4F}
-
-#define SECRET_CH_ID 1380146			// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "94AP16F80BD7MGN6"   // replace XYZ with your channel write API Key
